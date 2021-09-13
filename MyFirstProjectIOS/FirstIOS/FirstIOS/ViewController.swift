c17257f80cdf16b3022be69c3e33a11326772ffc
Vladimir Chebaturo <winipushkin777@icloud.com>
2021-09-13 16:40:26 +0300

    deletMain

--- MyFirstProjectIOS/FirstIOS/FirstIOS/ViewController.swift
+++ MyFirstProjectIOS/FirstIOS/FirstIOS/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lable = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue
+        
         // Do any additional setup after loading the view.
     }
 
