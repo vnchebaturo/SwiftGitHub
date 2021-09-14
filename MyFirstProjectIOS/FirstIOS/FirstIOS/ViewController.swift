2872494dfbafa2c97e34ec035a913f40c016fe88
Vladimir Chebaturo <winipushkin777@icloud.com>
2021-09-14 15:20:30 +0300

    AdditionAndGuessTheNumber14092021

--- MyFirstProjectIOS/FirstIOS/FirstIOS/ViewController.swift
+++ MyFirstProjectIOS/FirstIOS/FirstIOS/ViewController.swift
@@ -8,45 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-   // private let lable: UILabel = {
-     //  let lable = UILabel()
-        
-        //label2
-       // lable.text = "dgfbsdgfbasgf"
-        //return lable
-    //}()
+var label = UILabel()
+    var button = UIButton()
     override func viewDidLoad() {
-     
-        let label = UILabel(frame: CGRect(x: 60.0, y: 80.0, width: 150, height: 150))
-        label.center = self.view.center
-        label.text = "LABEL"
-        label.textColor = UIColor.blue
-        label.textAlignment = .center
-        label.backgroundColor = UIColor.green
-        label.numberOfLines = 5
-        label.lineBreakMode = .byCharWrapping
-        label.layer.cornerRadius = 75
-        label.clipsToBounds = true
+        
+        super.viewDidLoad()
+       // view.addSubview(button)
+        //showButton()
+        view.backgroundColor = .white
+        drawLabel(newLabel: label, newTextLabel: "Мое первое мини приложение", x: 0, y: 0, width: Double(UIScreen.main.bounds.width), height: 30, color: .clear, textAligment: .center)
+        label.center = CGPoint(x: view.center.x, y: 150)
+        // MARK: -Archor
+       // label.rightAnchor
         self.view.addSubview(label)
-        let str = "This is Label This is Label"
-        print(widthOfString(font: UIFont.systemFont(ofSize: 22), text: str))
+        additionButton(button: button, x: 30, y: 200, width: 100, height: 100, color: .brown)
         
-        
-        //self.viewDidLoad(label)
-       self.view.backgroundColor = UIColor.blue
-        
-        // Do any additional setup after loading the view.
-    }
-    func widthOfString (font:UIFont, text:String) -> CGFloat{
-        let attr = [NSAttributedString.Key.font:font]
-        let size = (text as NSString).size(withAttributes: attr)
-        return size.width
-    }
-//    private func setupLayot() {
-//        view.addSubview(lable)
+        self.view.addSubview(button)
 //
-//
-//    }
+  }
+     @objc func showButton () {
+        let aletController = UIAlertController(title: "Error", message: "NONONO", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) {(action) in
+        }
+        //aletController.addTextField(configurationHandler: nil)
+        aletController.addAction(action)
+        self.present(aletController, animated: true, completion: nil)
+    }
+    
+    func drawLabel(newLabel: UILabel, newTextLabel: String, x: Double, y: Double, width: Double, height: Double, color: UIColor, textAligment: NSTextAlignment) {
+            newLabel.frame = CGRect(x: x, y: y, width: width, height: height)
+            newLabel.text = newTextLabel
+            newLabel.textAlignment = textAligment
+        newLabel.backgroundColor = .red
+            newLabel.numberOfLines = 0
+            newLabel.lineBreakMode = .byWordWrapping
+        }
+    
+    func additionButton(button: UIButton, x: Double, y: Double, width: Double, height: Double, color: UIColor) {
+        button.frame = CGRect(x: x, y: y, width: width, height: height)
+        button.backgroundColor = color
+        button.setTitle("сложение", for: .normal)
+        button.addTarget(self, action: #selector(showButton), for: .touchUpInside)
+        button.layer.cornerRadius = 50.0
+        }
+    
 }
 
