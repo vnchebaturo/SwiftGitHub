480b916efc8ac811072a864dbfe7c1a431700bb0
Vladimir Chebaturo <winipushkin777@icloud.com>
2021-09-15 18:31:44 +0300

    15092021BackAPP

---
+++  UIKit4/UIKit4/UIKit4/FierstString.swift
@@ -0,0 +1,16 @@
+//
+//  FierstString.swift
+//  UIKit4
+//
+//  Created by Nikita on 15.09.2021.
+//
+
+import Foundation
+import UIKit
+
+class FierstString:UITableViewCell{
+    var label = UILabel()
+    
+}
+
+//cell.textLabel?.text = "This is row \(tableData[indexPath.row])"
---  UIKit4/UIKit4/UIKit4/ViewController.swift
+++  UIKit4/UIKit4/UIKit4/ViewController.swift
@@ -5,32 +5,51 @@
 //  Created by Nikita on 15.09.2021.
 //
 
+
 import UIKit
-import Foundation
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
     var tableView = UITableView()
     var tableData = ["Beach", "Clubs", "Chill", "Dance"]
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView = UITableView(frame: self.view.bounds, style: UITableView.Style.plain)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.backgroundColor = UIColor.white
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "my")
+//      let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let barHeight: CGFloat = UIApplication.shared.accessibilityFrame.size.height
+
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        tableView.contentInset.top = 20
+        tableView.frame = CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight)
+        let contentSize = self.tableView.contentSize
+        let footer = UIView(frame: CGRect(x: self.tableView.frame.origin.x,
+                                          y: self.tableView.frame.origin.y + contentSize.height,
+                                          width: self.tableView.frame.size.width,
+                                          height: self.tableView.frame.height - self.tableView.contentSize.height))
+        
+        self.tableView.tableFooterView = footer
+        
+        
         view.addSubview(tableView)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    @objc func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var fierstString = FierstString()
+        fierstString.label
+        return tableData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,4 +63,3 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return tableData.count
     }
 }
-
