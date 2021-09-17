254719b0f7418ccd0914a9ac0be97e9b992c5d85
Vladimir Chebaturo <winipushkin777@icloud.com>
2021-09-17 16:41:45 +0300

    17092021(1)

--- storyboad_stanford/katAndKard/katAndKard/ViewController.swift
+++ storyboad_stanford/katAndKard/katAndKard/ViewController.swift
@@ -8,18 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
 
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiChoices = ["🐥", "🧸", "🐥", "🧸"]
+
+    
     @IBAction func touchCard(_ sender: UIButton)  {
-        flipCard("🧸", on: sender)
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+                flipCard (withEmoji: emojiChoices[cardNumber], on: sender)
+                } else {
+                    print("12431 ")
+                }
     }
     
     
-    
-    func flipCard(_ emoji: String, on button: UIButton){
+    func flipCard(withEmoji emoji: String, on button: UIButton){
         if button.currentTitle == emoji{
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-           // button.setTitle("🧸", for: UIControl.State.normal)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
